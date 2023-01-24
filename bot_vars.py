0f50af6bcb12951dfb14c2833f16b0ebcd4ad734
Ethan Rickert <ethanrickert0429@gmail.com>
2023-01-24 06:35:15 -0500

    removed auto-generation of .env file

--- bot_vars.py
+++ bot_vars.py
@@ -2,17 +2,6 @@ import os
 import sys
 from dotenv import load_dotenv
 
-if os.path.exists('.env'):
-    pass
-else:
-    with open('.env', 'w') as f:
-        f.writelines([
-            '# .env\n',
-            'DISCORD_TOKEN=\n',
-        ])
-    print('.env file generated! Exiting bot script.\nFill the appropriate fields in the .env file before running the script again!')
-    sys.exit()
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 cppref = 'cppreference.com'
